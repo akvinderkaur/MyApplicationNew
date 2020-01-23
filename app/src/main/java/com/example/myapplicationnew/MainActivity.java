e4ffdc9df6d3d783929cf753e0f04564664e70d4
1895975 <Akvinderkaur01021994@gmail.com>
2020-01-23 16:34:38 -0500

    saffdsg

---
+++ app/src/main/java/com/example/myapplicationnew/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplicationnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_call;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_call= findViewById(R.id.btn_call);
+
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               /* Snackbar snb= Snackbar.make(findViewById(R.id.myView),"Done!",Snackbar.LENGTH_LONG);
+                snb.show();*/
+
+               Snackbar snb= Snackbar.make(findViewById(R.id.myView),"Action Snackbar!", Snackbar.LENGTH_INDEFINITE).setAction("Retry!", new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       Toast.makeText(getApplicationContext(), "Snackbar Toast", Toast.LENGTH_LONG).show();
+                   }
+               });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplicationnew/aaa.java
@@ -0,0 +1,4 @@
+package com.example.myapplicationnew;
+
+public class aaa {
+}
